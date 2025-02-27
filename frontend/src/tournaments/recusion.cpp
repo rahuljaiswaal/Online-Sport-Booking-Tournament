ff5c242a7e7d6b87e189aa53fe25886f2bd385b3
Rahul <rahul770088@gmail.com>
2025-02-27 11:57:21 +0530

    Initial commt

--- frontend/src/tournaments/recusion.cpp
+++ frontend/src/tournaments/recusion.cpp
@@ -8,16 +8,6 @@ int main()
     vector<int> v = {7,4,8,2,9};
     cout<<"hello"<<endl;
 	vector<int> ans;
-	// stack<int> st;
-	// if(v.size()>0){ ans.push_back(v[0]);
-	// st.push(v[0]);}
-	// for(int i=1; i<v.size(); i++){
-	// 	while(st.size()>0 && st.top()<v[i]){
-	// 		st.pop();
-	// 	}
-	// 	if(st.size()==0) ans.push_back(v[i]);
-	// 	st.push(v[i]);
-	// }
     int max = v[0];
     ans.push_back(v[0]);
     for(int i=1; i<v.size(); i++){
